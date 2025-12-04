226e8b895d46b00ac84e66c881e882b247e38bc3
Alexandre Vernet <alexandre.vernet99@gmail.com>
2025-12-04 20:00:37 +0100

    feat: Update camera position

--- src/app/camera.ts
+++ src/app/camera.ts
@@ -2,7 +2,8 @@ import * as T from 'three';
 
 export const camera = () => {
   const camera = new T.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  camera.position.set(0, 15, 5);
+  camera.position.set(-80, 1, 5);
+  camera.rotation.y = -Math.PI / 2;
 
   return camera;
 }
